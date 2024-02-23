bc6b2b166093ae90826b82612e7ec3906a178ec7
Tai Nguyen-Trung (Teth Adam) <tai.nguyentrung1996@gmail.com>
2024-02-23 17:00:41 +0700

    Add React Route - initial UI Components - Page

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Navbar = () => {
+	return (
+		<div className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+			<h2 className="text-2xl font-semibold">Food Receipe</h2>
+      <form>
+        
+      </form>
+		</div>
+	);
+};
